6b82b4e83b31c6657a80ff6d089f37aa16382aac
AndreasBakke <andreas.s.bakke@gmail.com>
2021-01-15 16:35:24 +0100

    test

--- strava/wsgi.py
+++ strava/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strava.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strava.settings.stravakeaderboard')
 
 application = get_wsgi_application()
