f9b8e57b42ad3436322e14b3882d1ceab77cc67f
AndreasBakke <andreas.s.bakke@gmail.com>
2021-01-15 18:06:25 +0100

    test

--- strava/settings.py
+++ strava/settings.py
@@ -121,7 +121,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
