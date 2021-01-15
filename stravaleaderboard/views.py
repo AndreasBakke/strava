cffbb15bdcf0a7167c99fbf1ee65548bae938e07
AndreasBakke <andreas.s.bakke@gmail.com>
2021-01-15 20:13:30 +0100

    test

--- stravaleaderboard/views.py
+++ stravaleaderboard/views.py
@@ -3,6 +3,7 @@ from .models import Club, Distances
 from django.http import HttpResponse
 from .updates import update
 import datetime
+import time
 
 
 # Create your views here.
@@ -11,8 +12,9 @@ def index(request):
     club_name_list = Club.objects.all()
     #template = loader.get_template('stravaleaderboard/index.html')
     last_updates = club_name_list[0].last_update
-    last_updatesD = datetime.datetime.fromtimestamp(last_updates)
     dato = datetime.datetime.today()
+    t = int(time.time())
+    deltaT = t-last_updates
     startdate = datetime.datetime(2021, 1, 4)#update to 11 when publishing
     for i in range(1,11):
         if startdate + datetime.timedelta(days=7)*(1+i)>= dato >= startdate + datetime.timedelta(days=7)*i:
@@ -20,7 +22,7 @@ def index(request):
     context = {
         'club_name_list' : club_name_list,
         'current_week': currentWeek,
-        'last_update': last_updatesD
+        'deltaT': deltaT
     }
     return render(request, 'stravaleaderboard/index.html', context)
 
