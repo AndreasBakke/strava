5c373e601c10ffd6a96b3efea16799aa36028c9d
AndreasBakke <andreas.s.bakke@gmail.com>
2021-01-19 09:37:25 +0100

    Added bike config

--- strava/settings.py
+++ strava/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import django_heroku
 from pathlib import Path
 import os
-import whitenoise
+import whitenoise 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
