bb13bd90beeeecfd51641d4060c037254491c6f2
AndreasBakke <andreas.s.bakke@gmail.com>
2021-01-19 10:25:36 +0100

    Added bike config

--- strava/settings.py
+++ strava/settings.py
@@ -30,6 +30,21 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 # Application definition
 
