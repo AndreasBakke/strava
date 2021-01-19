4f893c8190eb095df43068610edf1927f7b41195
AndreasBakke <andreas.s.bakke@gmail.com>
2021-01-19 12:39:48 +0100

    test

--- strava/urls.py
+++ strava/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('stravaleaderboard/', include('stravaleaderboard.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
