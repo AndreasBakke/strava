f5613914f88dd5404dbd79e4130f71027a31c4d1
AndreasBakke <andreas.s.bakke@gmail.com>
2021-01-19 11:18:01 +0100

    test

--- strava/settings.py
+++ strava/settings.py
@@ -136,7 +136,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
