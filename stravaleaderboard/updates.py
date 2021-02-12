c4ff053218a8ff6d682ca4f9042184b1744a7640
AndreasBakke <andreas.s.bakke@gmail.com>
2021-02-12 23:20:59 +0100

    New Styling and rules

--- stravaleaderboard/updates.py
+++ stravaleaderboard/updates.py
@@ -90,7 +90,7 @@ def update():
         updatetime = club_name_list[0].last_update
 
 
-    if (t-updatetime)>1800:
+    if (t-updatetime)>3600:
         exchange_credentials()
         Create_check_Clubs() 
         update_distances()
