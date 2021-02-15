e88cccf1160cfd82e42c5ea90be1044debbd4e2e
AndreasBakke <andreas.s.bakke@gmail.com>
2021-02-15 13:38:00 +0100

    krise

--- stravaleaderboard/views.py
+++ stravaleaderboard/views.py
@@ -15,7 +15,7 @@ def index(request):
     dato = datetime.datetime.today()
     t = int(time.time())
     deltaT = int((t-last_updates)/60)
-    startdate = datetime.datetime(2021, 2, 1)#update to 19 when publishing
+    startdate = datetime.datetime(2021, 18, 1)#update to 19 when publishing
     for i in range(0,11):
         if startdate + datetime.timedelta(days=7)*(1+i)>= dato >= startdate + datetime.timedelta(days=7)*i:
             currentWeek=i
