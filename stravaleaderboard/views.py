193a8368e01cab7cdf9bb9d4767f2090e34c6016
AndreasBakke <andreas.s.bakke@gmail.com>
2021-04-07 15:29:57 +0200

    Ahh fix

--- stravaleaderboard/views.py
+++ stravaleaderboard/views.py
@@ -8,6 +8,7 @@ import time
 
 # Create your views here.
 def index(request):
+    currentweek = 10
     update()
     club_name_list = Club.objects.all().order_by('-currentpoints')
     #template = loader.get_template('stravaleaderboard/index.html')
