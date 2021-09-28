60353665f7377c26d4b9ef3044ea058f779bf546
Andreas <andreas.s.bakke@gmail.com>
2021-09-28 08:59:02 +0200

    test

--- stravaleaderboard/updates.py
+++ stravaleaderboard/updates.py
@@ -24,7 +24,7 @@ def update_distances():
     t = int(time.time())
     a= Secret.objects.get(name = "strava")
     dato = datetime.datetime.today()
-    startdate = datetime.datetime(2021, 1, 18)#update to 18 when publishing
+    startdate = datetime.datetime(2021, 9, 18)#update to 18 when publishing
     for i in range(1,10):
         if startdate + datetime.timedelta(days=7)*(1+i)>= dato >= startdate + datetime.timedelta(days=7)*i:
             currentWeek =i
@@ -97,7 +97,7 @@ def update():
         updatetime = club_name_list[0].last_update
 
 
-    if (t-updatetime)>3600:
+    if (t-updatetime)>7200:
         exchange_credentials()
         Create_check_Clubs() 
         update_distances()
