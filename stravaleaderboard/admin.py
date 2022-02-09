b465b7872d467b0ed614a4179a5c1e301ab6fa4b
andreasbakke <andreas.s.bakke@gmail.com>
2022-02-09 18:29:21 +0100

    Added distance reset to admin page

--- stravaleaderboard/admin.py
+++ stravaleaderboard/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Club, Distances, Secret
 # Register your models here.
+
+@admin.action(description='Reset distance back to 0')
+def resetDistance(modeladmin, request, queryset):
+    queryset.update(distance=0)
+    
+class DistanceAdmin(admin.ModelAdmin):
+    actions = [resetDistance]
+
 admin.site.register(Club)
-admin.site.register(Distances)
+admin.site.register(Distances, DistanceAdmin)
 admin.site.register(Secret)
+ 
