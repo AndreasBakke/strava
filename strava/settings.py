a092615069a83c5c3c1f1cc32f0eeb440eb3358f
AndreasBakke <andreas.s.bakke@gmail.com>
2022-06-22 08:19:23 +0200

    Update settings.py

--- strava/settings.py
+++ strava/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^pon-p+5o7+fm18e-%yjre1&uv8e^x$d))i9t!n1_hmz0093mg'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
